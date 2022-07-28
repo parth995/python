a404a8b6568fe4d3f53af459e8ab50e257b864bb
Parth Patel <parth.patel@zscaler.com>
2022-07-28 17:06:12 +0530

    minor fix for socket program.

--- tasks/udp.py
+++ tasks/udp.py
@@ -17,14 +17,21 @@ def server(port):
 
 def client(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    hosts = []
     while True:
-        message = input('Input lowercase sentence:' )
+        host = input('Input host address:' )
+        hosts.append((host,port))
+        message = input('Input message to send to server:' )
         data = message.encode('ascii')
-        s.sendto(data, ('127.0.0.1', port))
+        s.sendto(data, (host, port))
         print('The OS assigned the address {} to me'.format(s.getsockname()))
-        data, address = s.recvfrom(MAX_SIZE_BYTES) 
+        data, address = s.recvfrom(MAX_SIZE_BYTES)
         text = data.decode('ascii')
-        print('The server {} replied with {!r}'.format(address, text))
+        if(address in hosts):
+            print('The server {} replied with {!r}'.format(address, text))
+            hosts.remove(address)
+        else:
+            print('message {!r} from unexpected host {}!'.format(text, address))
 
 if __name__ == '__main__':
     funcs = {'client': client, 'server': server}
